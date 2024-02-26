0e4c2bfa3f5d53023df0a89fb8803f3d5adeea1c
lisa7012 <71042901+lisa7012@users.noreply.github.com>
2024-02-26 00:53:57 -0500

    Add draft landing page with styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <main className="">
-      <h2>eGreet</h2>
+    <main className="h-screen bg-landing-pattern px-50px m-0 pt-11 pb-0">
+      <Image
+          src="/eGreet_logo_gray.svg"
+          alt="eGreet Gray Logo"
+          width={153}
+          height={87}
+        />
+        <div className="flex gap-12 mx-72px">
+          <div className="self-center w-785px flex flex-col gap-7">
+            <p className="text-2xl text-white self-center font-normal">
+              Need to send card, but don&apos;t have time to run to the store? At eGreet, you may send free e-cards to your friends, family, co-workers, anyone! Simply browse for a photo you want from the photo gallery, customize your message, and send away!
+            </p>
+            <Link href="/photogallery" className="px-24 py-3 bg-mustard w-300px font-medium text-center rounded-md text-lg">get started!</Link>
+          </div>
+          <div>
+            <Image
+              src="/eCards_example.svg"
+              alt="eCards example"
+              width={756}
+              height={675}
+            />
+          </div>
+        </div>
+        <footer className="text-xl text-white flex justify-center mb-10 mt-11 opacity-70">
+            ©2024 | designed by Victoria Marins & developed by Lisa Duong
+        </footer>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        strawberry: {
+          400: "#F46C83",
+          600: "#DD415B",
+        },
+        mustard: "#ECBD2C",
+        tangerine: "#E48659",
+        'white-ivory': "#E48659",
+        'white': "#FFFFFF",
+        'black': "#333333",
       },
+      backgroundImage: {
+        'landing-pattern': "url('/landing_pattern.svg'), linear-gradient(135deg, rgba(244,108,131,1) 0%, rgba(221,65,91,1) 35%, rgba(228,134,89,1) 100%)",
+      },
+      backgroundSize: {
+        '100%': 'auto 100%',
+      },
+      fontFamily: {
+        poppins: 'var(--font-poppins)'
+      },
+      padding: {
+        '50px': '3.125rem',
+      },
+      margin: {
+        '72px': '4.5rem',
+      },
+      width:{
+        '300px': '18.75rem',
+        '785px': '49.063rem',
+      }
     },
   },
   plugins: [],
