84b47d928942e5b40a78bae031f29008957bebe0
lisa7012 <71042901+lisa7012@users.noreply.github.com>
2024-02-27 00:27:34 -0500

    Fix format and add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,79 +2,79 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Home = () => {
-	return (
-		<main className='h-screen z-0 bg-gradient m-0 py-0 relative'>
-			{/* Origami pattern in the background */}
-			<section className='h-screen w-screen -z-1 fixed pt-0 grid grid-rows-12 grid-cols-12'>
-				<Image
-					src='/pattern-g1.svg'
-					alt='pattern group 1'
-					width={438}
-					height={490}
-					className='col-span-3 row-start-8 pt-3'
-				/>
-				<Image
-					src='/pattern-g2.svg'
-					alt='pattern group 2'
-					width={521}
-					height={688}
-					className='col-span-4 row-span-6 col-start-4 row-start-7 mt-11'
-				/>
-				<Image
-					src='/pattern-g3.svg'
-					alt='pattern group 3'
-					width={756}
-					height={361}
-					className='col-span-5 row-span-4 row-start-1 col-start-7'
-				/>
-				<Image
-					src='/pattern-g4.svg'
-					alt='pattern group 4'
-					width={521}
-					height={688}
-					className='col-span-5 row-span-8 row-start-5 col-start-10'
-				/>
-			</section>
-			{/* TODO: Make separate component or similar to add to layout.tsx and figure out how to grab params to put the right logo */}
-			<Link href='/'>
-				<Image
-					src='/eGreet_logo_gray.svg'
-					alt='eGreet Gray Logo'
-					width={153}
-					height={87}
-					className='pt-11 ml-50px'
-				/>
-			</Link>
-			<div className='flex gap-12 mx-auto justify-center relative'>
-				<div className='self-center w-785px flex flex-col gap-7'>
-					<p className='text-3xl text-white self-center font-light'>
-						Need to send a card, but don&apos;t have time to run to the store?
-						At eGreet, you may send free e-cards to your friends, family,
-						co-workers, anyone! Simply browse for a photo you want from the
-						photo gallery, customize your message, and send away!
-					</p>
-					<Link
-						href='/photogallery'
-						className='px-24 py-3 bg-mustard w-300px font-medium text-center rounded-md text-lg hover:shadow-lg hover:animate-bounce motion-reduce:animate-bounce'
-					>
-						get started!
-					</Link>
-				</div>
-				<div>
-					<Image
-						src='/eCards_example.svg'
-						alt='eCards example'
-						width={756}
-						height={675}
-					/>
-				</div>
-			</div>
-			{/* TODO: separate component for footer and to put in layout.tsx */}
-			<footer className='text-xl text-white flex justify-center mb-10 mt-11 opacity-70 font-light'>
-				©2024 | designed by Victoria Marins & developed by Lisa Duong
-			</footer>
-		</main>
-	);
+  return (
+    <main className="relative z-0 m-0 h-screen bg-gradient py-0">
+      {/* Origami pattern in the background */}
+      <section className="fixed -z-1 grid h-screen w-screen grid-cols-12 grid-rows-12 pt-0">
+        <Image
+          src="/pattern-g1.svg"
+          alt="pattern group 1"
+          width={438}
+          height={490}
+          className="col-span-3 row-start-8 pt-3"
+        />
+        <Image
+          src="/pattern-g2.svg"
+          alt="pattern group 2"
+          width={521}
+          height={688}
+          className="col-span-4 col-start-4 row-span-6 row-start-7 mt-11"
+        />
+        <Image
+          src="/pattern-g3.svg"
+          alt="pattern group 3"
+          width={756}
+          height={361}
+          className="col-span-5 col-start-7 row-span-4 row-start-1"
+        />
+        <Image
+          src="/pattern-g4.svg"
+          alt="pattern group 4"
+          width={521}
+          height={688}
+          className="col-span-5 col-start-10 row-span-8 row-start-5"
+        />
+      </section>
+      {/* TODO: Make separate component or similar to add to layout.tsx and figure out how to grab params to put the right logo */}
+      <Link href="/">
+        <Image
+          src="/eGreet_logo_gray.svg"
+          alt="eGreet Gray Logo"
+          width={153}
+          height={87}
+          className="ml-50px pt-11"
+        />
+      </Link>
+      <div className="relative mx-auto flex justify-center gap-12">
+        <div className="flex w-785px flex-col gap-7 self-center">
+          <p className="self-center text-3xl font-light text-white">
+            Need to send a card, but don&apos;t have time to run to the store?
+            At eGreet, you may send free e-cards to your friends, family,
+            co-workers, anyone! Simply browse for a photo you want from the
+            photo gallery, customize your message, and send away!
+          </p>
+          <Link
+            href="/photogallery"
+            className="w-300px rounded-md bg-mustard px-24 py-3 text-center text-lg font-medium hover:animate-bounce hover:shadow-lg motion-reduce:animate-bounce"
+          >
+            get started!
+          </Link>
+        </div>
+        <div>
+          <Image
+            src="/eCards_example.svg"
+            alt="eCards example"
+            width={756}
+            height={675}
+          />
+        </div>
+      </div>
+      {/* TODO: separate component for footer and to put in layout.tsx */}
+      <footer className="mb-10 mt-11 flex justify-center text-xl font-light text-white opacity-70">
+        ©2024 | designed by Victoria Marins & developed by Lisa Duong
+      </footer>
+    </main>
+  );
 };
 
 export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
         strawberry: {
-          400: "#F46C83",
-          600: "#DD415B",
+          400: '#F46C83',
+          600: '#DD415B',
         },
-        mustard: "#ECBD2C",
-        tangerine: "#E48659",
-        'white-ivory': "#E48659",
-        'white': "#FFFFFF",
-        'black': "#333333",
+        mustard: '#ECBD2C',
+        tangerine: '#E48659',
+        'white-ivory': '#E48659',
+        white: '#FFFFFF',
+        black: '#333333',
       },
       backgroundImage: {
-        'gradient': "linear-gradient(135deg, rgba(244,108,131,1) 0%, rgba(221,65,91,1) 32%, rgba(228,134,89,1) 80%)",
+        gradient:
+          'linear-gradient(135deg, rgba(244,108,131,1) 0%, rgba(221,65,91,1) 32%, rgba(228,134,89,1) 80%)',
       },
       fontFamily: {
-        poppins: 'var(--font-poppins)'
+        poppins: 'var(--font-poppins)',
       },
       padding: {
         '50px': '3.125rem',
@@ -32,7 +33,7 @@ const config: Config = {
         '72px': '4.5rem',
         '50px': '3.125rem',
       },
-      width:{
+      width: {
         '300px': '18.75rem',
         '785px': '49.063rem',
       },
