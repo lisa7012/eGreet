bba0c8698a088898ca178ac9cea6d2a0578079f2
lisa7012 <71042901+lisa7012@users.noreply.github.com>
2024-03-02 15:55:13 -0500

    Add responsiveness to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Home = () => {
   return (
-    <main className="relative z-0 m-0 h-screen bg-gradient py-0">
+    <main className="relative z-0 mx-0 my-auto flex h-screen flex-col bg-gradient py-0 max-sm:gap-2.5">
       {/* Origami pattern in the background */}
       <section className="fixed -z-1 grid h-screen w-screen grid-cols-12 grid-rows-12 pt-0">
         <Image
@@ -11,56 +11,58 @@ const Home = () => {
           alt="pattern group 1"
           width={438}
           height={490}
-          className="col-span-3 row-start-8 pt-3"
+          className="col-span-3 row-start-8 pt-3 max-lg:row-start-10 max-mobile:hidden"
         />
         <Image
           src="/pattern-g2.svg"
           alt="pattern group 2"
           width={521}
           height={688}
-          className="col-span-4 col-start-4 row-span-6 row-start-7 mt-11"
+          className="col-span-4 col-start-4 row-span-6 row-start-7 mt-11 max-mobile:hidden"
         />
         <Image
           src="/pattern-g3.svg"
           alt="pattern group 3"
           width={756}
           height={361}
-          className="col-span-5 col-start-7 row-span-4 row-start-1"
+          className="col-span-5 col-start-7 row-span-4 row-start-1 max-mobile:col-span-9 max-mobile:col-start-4"
         />
         <Image
           src="/pattern-g4.svg"
           alt="pattern group 4"
           width={521}
           height={688}
-          className="col-span-5 col-start-10 row-span-8 row-start-5"
+          className="col-span-5 col-start-10 row-span-8 row-start-5 max-lg:row-start-10 max-mobile:col-span-9 max-mobile:col-start-4 max-mobile:row-start-7"
         />
       </section>
       {/* TODO: Make separate component or similar to add to layout.tsx and figure out how to grab params to put the right logo */}
-      <Link href="/">
-        <Image
-          src="/eGreet_logo_gray.svg"
-          alt="eGreet Gray Logo"
-          width={153}
-          height={87}
-          className="ml-50px pt-11"
-        />
-      </Link>
-      <div className="relative mx-auto flex justify-center gap-12">
-        <div className="flex w-785px flex-col gap-7 self-center">
-          <p className="self-center text-3xl font-light text-white">
+      <div className="ml-50px mt-11 inline-block h-logo-cl w-logo-cl max-lg:ml-logo-x-cl max-lg:mt-logo-y-cl">
+        <Link href="/" className=" inline-block">
+          <Image
+            src="/eGreet_logo_gray.svg"
+            alt="eGreet Gray Logo"
+            width={153}
+            height={87}
+          />
+        </Link>
+      </div>
+      <div className=" my-auto flex justify-center gap-content-cl px-content-cl max-lg:flex-wrap-reverse">
+        <div className="max-lg:gap-intro-cl flex w-785px flex-col gap-7 self-center">
+          <p className="self-center text-3xl font-normal text-white max-xl:text-intro-cl max-lg:text-center max-lg:leading-intro-cl">
             Need to send a card, but don&apos;t have time to run to the store?
             At eGreet, you may send free e-cards to your friends, family,
             co-workers, anyone! Simply browse for a photo you want from the
             photo gallery, customize your message, and send away!
           </p>
+          {/* TODO: Make a button component with general style that can be changed */}
           <Link
             href="/photogallery"
-            className="w-300px rounded-md bg-mustard px-24 py-3 text-center text-lg font-medium hover:animate-bounce hover:shadow-lg motion-reduce:animate-bounce"
+            className="w-fit rounded-md bg-mustard px-24 py-3 text-center text-lg font-medium hover:animate-bounce hover:shadow-lg motion-reduce:animate-bounce max-xl:px-btn-x-cl max-xl:py-btn-y-cl max-xl:text-btn-cl max-xl:leading-snug max-lg:self-center"
           >
             get started!
           </Link>
         </div>
-        <div>
+        <div className="max-lg:w-example-cl">
           <Image
             src="/eCards_example.svg"
             alt="eCards example"
@@ -70,7 +72,7 @@ const Home = () => {
         </div>
       </div>
       {/* TODO: separate component for footer and to put in layout.tsx */}
-      <footer className="mb-10 mt-11 flex justify-center text-xl font-light text-white opacity-70">
+      <footer className="mb-10 mt-11 flex justify-center text-xl font-normal text-white opacity-70 max-xl:text-footer-cl">
         ©2024 | designed by Victoria Marins & developed by Lisa Duong
       </footer>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config: Config = {
         white: '#FFFFFF',
         black: '#333333',
       },
+      screens: {
+        mobile: '480px',
+      },
       backgroundImage: {
         gradient:
           'linear-gradient(135deg, rgba(244,108,131,1) 0%, rgba(221,65,91,1) 32%, rgba(228,134,89,1) 80%)',
@@ -26,16 +29,36 @@ const config: Config = {
       fontFamily: {
         poppins: 'var(--font-poppins)',
       },
+      fontSize: {
+        'intro-cl': 'clamp(0.875rem, 0.4606rem + 1.7680vw, 1.875rem)',
+        'btn-cl': 'clamp(0.813rem, 0.6837rem + 0.5516vw, 1.125rem)',
+        'footer-cl': 'clamp(0.625rem, 0.3660rem + 1.1050vw, 1.25rem)',
+      },
+      lineHeight: {
+        'intro-cl': 'clamp(1.313rem, 0.7716rem + 2.3100vw, 2.25rem)',
+      },
       padding: {
-        '50px': '3.125rem',
+        'btn-x-cl': 'clamp(5.438rem, 5.2051rem + 0.9936vw, 6rem)',
+        'btn-y-cl': 'clamp(0.5rem, 0.3964rem + 0.4420vw, 0.75rem)',
+        'content-cl': 'clamp(1.875rem, 1.1527rem + 3.0817vw, 3.125rem)',
       },
       margin: {
-        '72px': '4.5rem',
         '50px': '3.125rem',
+        'logo-y-cl': 'clamp(1.875rem, 1.3694rem + 2.1572vw, 2.75rem)',
+        'logo-x-cl': 'clamp(1.875rem, 1.1527rem + 3.0817vw, 3.125rem)',
       },
       width: {
-        '300px': '18.75rem',
         '785px': '49.063rem',
+        'logo-cl': 'clamp(4.438rem, 3.1941rem + 5.3074vw, 9.563rem)',
+        'btn-cl': 'clamp(15.625rem, 14.3301rem + 5.5249vw, 18.75rem)',
+        'example-cl': 'clamp(18.75rem, 12.2496rem + 27.7350vw, 30rem)',
+      },
+      height: {
+        'logo-cl': 'clamp(2.063rem, 1.2438rem + 3.4951vw, 5.438rem)',
+      },
+      gap: {
+        'content-cl': 'clamp(1.875rem, 1.2250rem + 2.7735vw, 3rem)',
+        'intro-cl': 'clamp(1.25rem, 1.0428rem + 0.8840vw, 1.75rem)',
       },
       zIndex: {
         '1': '1',
