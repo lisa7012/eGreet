e5089d2877b2715078fb4dfbd19e820f8dc6e586
lisa7012 <71042901+lisa7012@users.noreply.github.com>
2024-04-10 01:24:52 -0400

    Fix format

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -21,9 +21,7 @@ const Footer = ({
   return (
     <footer className="pt-auto flex justify-center pb-10">
       <p className={style}>
-        <sup className="text-copyright-cl inline-block self-start pt-0 leading-none">
-          ©
-        </sup>
+        <sup className="inline-block·self-start·pt-0·text-copyright-cl">©</sup>
         2024 | designed and developed by&nbsp;
         <Link
           href="https://www.linkedin.com/in/vict%C3%B3ria-marins-4700b01ba?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BHXdYazhwSkC4nyDPNx3pOg%3D%3D"
